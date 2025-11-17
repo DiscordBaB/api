37cb969e4b22836869fa69d41acebc74736f5599
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-11-17 12:16:27 -0500

    feat: update add models

---
+++ models/ACLModel.js
@@ -0,0 +1,45 @@
+const { SQLize, sqlize, Model } = require('../databases/db')
+class ACL extends Model {
+
+}
+ACL.init(
+    {
+        id: {
+            type: SQLize.INTEGER,
+            unique: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        key: {
+            type: SQLize.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        createdAt: {
+            type: SQLize.DATE,
+            allowNull: false,
+        },
+        permissions: {
+            type: SQLize.JSON,
+            allowNull: false,
+            /*
+            * {
+            *   api: [true, {}],
+            *   dashboard: true,
+            *
+            */
+        }
+    },
+    {
+        sqlize,
+        freezeTableName: true,
+        tableName: 'ACL',
+        modelName: 'ACL',
+        timestamps: true
+    }
+
+);
+(async () => {
+    await ACL.sync()
+})();
+module.exports = ACL

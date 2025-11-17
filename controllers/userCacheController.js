af27f145ee2e2f0f0bac60bc74db1fdcae6f8e97
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-11-17 13:00:04 -0500

    feat: add controller sql for getUser

--- controllers/userCacheController.js
+++ controllers/userCacheController.js
@@ -3,8 +3,13 @@ exports.getUser = async function (req, res) {
     let server_id, user_id;
     server_id = req.body.server_id
     user_id = req.body.user_id
-    const { rows } = await userCache.findByPk(user_id, (err, acl_result) =>{
-        if (err) return res.status(500).send({code: 1, message: SQL_ERROR});
+    const { rows } = await userCache.findAll({
+                where: {
+                    server_id: server_id,
+                    user_id: user_id
+                }
+            }, (err, _) => {
+        if (err) return res.status(500).send({code: 1, message: 'SQL_ERROR'});
         res.status(200).json(rows)
     })
 }

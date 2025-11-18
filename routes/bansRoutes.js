6fd348c4de44856a3c46faca8b2deba7aecac22e
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-11-18 13:16:40 -0500

    feat: add route for adding bans for servers

--- routes/bansRoutes.js
+++ routes/bansRoutes.js
@@ -4,4 +4,5 @@ const banController = require('../controllers/BansController');
 
 router.get('/bans/:server_id', appealController.getAllBansByServer);
 router.get('/bans/:server_id/:user_id')
+router.post('/bans/:server_id', banController.addBantoServer)
 module.exports = router;

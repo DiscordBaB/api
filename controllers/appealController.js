7b338a63c206c5a50c095c613623e7d3f873919f
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-11-28 10:13:16 -0500

    feat: update appealController.js with delete

--- controllers/appealController.js
+++ controllers/appealController.js
@@ -1,17 +1,26 @@
+
 const Appeal = require('../models/appealModel');
 exports.getAllAppeals = async function (req, res) {
     let limit = req.body.limit
-    let offset = req.body.offset
-    const {count, rows } = await Appeal.findAndCountAll({limit: limit, offset: offset}, (err, appeals) => {
+    let page = req.body.page
+    const {count, rows } = await Appeal.findAndCountAll({limit: limit, offset: page*limit}, (err, appeals) => {
         if (err) return res.status(500).send({code: 1, message: "SQL_ERROR"});
         res.status(200).json(rows);
     });
 };
-exports.getAppeal = async function (req, res) {
-    let appeal_id = req.body.appeal_id
-    const { row } = await Appeal.findByPk(appeal_id, (err, appeal) =>{
-        if (err) return res.status(500).send({code: 1, message: SQL_ERROR});
-        res.status(200).json(row)
-    })
 
-}
+exports.getAppeal = async function (req, res) {
+    let appeal_id = req.appeal_id
+    const { row } = await Appeal.findByPk(appeal_id, (err, appeal) =>{
+        if (err) return res.status(500).send({code: 1, message: 'SQL_ERROR'});
+        res.status(200).json(row)
+    });
+};
+
+exports.deleteAppeal = async function (req, res) {
+    let appeal_id = req.appeal_id
+    const { row } = await Appeal.destroy({where: {id: appeal_id}}, (err, appeal) => {
+        if (err) return res.status(500).send({code: 1, message: 'SQL_ERROR'});
+        res.status(200).json({message: 'APPEAL_DELETED'})
+    });
+};

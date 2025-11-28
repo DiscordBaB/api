569529baa6b99325df432617e6b8c33977276c02
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-11-28 10:23:33 -0500

    feat: update appealsRoutes.js and add validation

--- routes/appealsRoutes.js
+++ routes/appealsRoutes.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const appealController = require('../controllers/appealController');
 const { body, validationResult, param, query } = require('express-validator');
 
-router.get('/appeals/', appealController.getAllAppeals);
-router.get('/appeals/:id', appealController.getAppeal)
+router.get('/appeals/', [
+    param('appeal_id').isString().notEmpty().withMessage('appeal_id is required and must be a string'),
+    body('limit').optional().isInt({ min: 1, max: 10 }).withMessage('limit must be an integer between 1 and 10'),
+    body('page').optional().isInt({min: 1, max: 1000}).withMessage('page must be an integer between 1 and 1000'),
+    appealController.getAllAppeals
+]);
+router.get('/appeals/:appeal_id', [
+    appealController.getAppeal
+]);
+router.delete('/appeals/:appeal_id', [
+    appealController.deleteAppeal,
+]);
 module.exports = router;

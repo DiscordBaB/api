eb01f8497932c569bf90ebf6d5da46b2d8befdca
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-02 16:17:36 -0500

    feat: update app.js to include all applicable routes

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ const fs = require('node:fs');
 const path = require('node:path');
 // app initialization
 const app = express();
-// use only json
+// use only JSON
 app.use(express.json());
 // middlewares
 app.use(morgan) // logging first
 app.use(AuthHeaderCheck)
 
-let routes = require('./routes/appealsRoutes')
-//app.use('/appeal')
+let AppealsRouter = require('./routes/appealsRoutes')
+let BansRouter = require('./routes/bansRoutes')
+let CacheRouter = require('./routes/cacheRoutes')
+let ACLRouter = require('./routes/ACLRoutes')
+let BansCountRouter = require('./routes/bansCountRoutes')
+
+
+app.use('/appeals', AppealsRouter)
+app.use('/bans', BansRouter)
+app.use('/cache', CacheRouter)
+app.use('/acl', ACLRouter)
+app.use('/bc', BansCountRouter)
 
 module.exports = app

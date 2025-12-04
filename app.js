56c2dfbaa646ed59720afdcf31f52c7a8a460e5e
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-04 11:43:42 -0500

    feat: migrate to ES modules and update dependencies

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const logger = require('./middlewares/morgan')
-const  AuthHeaderCheck = require( './middlewares/auth')
-const fs = require('node:fs');
-const path = require('node:path');
+import express, { json } from 'express';
+import logger from './middlewares/morgan.cjs';
+import AuthHeaderCheck from './middlewares/auth.js';
+import fs from 'node:fs';
+import path from 'node:path';
 // app initialization
 const app = express();
 // use only JSON
-app.use(express.json());
+app.use(json());
 // middlewares
 app.use(logger())
 app.use(AuthHeaderCheck)
 
-let AppealsRouter = require('./routes/appealsRoutes')
-let BansRouter = require('./routes/bansRoutes')
-let CacheRouter = require('./routes/cacheRoutes')
-let ACLRouter = require('./routes/ACLRoutes')
-let BansCountRouter = require('./routes/bansCountRoutes')
+import AppealsRouter from './routes/appealsRoutes.js';
+import BansRouter from './routes/bansRoutes.js';
+import CacheRouter from './routes/cacheRoutes.js';
+import ACLRouter from './routes/ACLRoutes.js';
+import BansCountRouter from './routes/bansCountRoutes.js';
 
 
 app.use('/appeals', AppealsRouter)
@@ -24,4 +24,4 @@ app.use('/cache', CacheRouter)
 app.use('/acl', ACLRouter)
 app.use('/bc', BansCountRouter)
 
-module.exports = app
+export default app

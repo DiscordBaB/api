54c15d3811fc7ce6a582599e042b463b145d4c57
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-05 13:05:40 -0500

    feat: update route to include user_id and server_id parameters in ACL routes

--- routes/ACLRoutes.js
+++ routes/ACLRoutes.js
@@ -4,5 +4,5 @@ import * as ACLController from '../controllers/ACLController.js';
 import { body, validationResult, param, query } from 'express-validator';
 
 router.get('/', ACLController.getAllACLs);
-router.get('/:id')
+router.get('/:user_id/:server_id', [ACLController.getACLByServerAndUserId]);
 export default router;

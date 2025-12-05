bc4c2014da12bf5053176dca2957d2e47105d75c
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-05 13:06:05 -0500

    feat: add user-specific ban retrieval route in bansRoutes

--- routes/bansRoutes.js
+++ routes/bansRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 const router = Router();
-import { getAllBansByServer, addBantoServer } from '../controllers/BansController.js';
+import { getAllBansByServer, addBantoServer, getBansforUserByServer } from '../controllers/BansController.js';
 import { body, validationResult, param, query } from 'express-validator';
 
 
 router.get('/:server_id', [getAllBansByServer]);
-router.get('/:server_id/:user_id')
-router.post('/:server_id', addBantoServer)
+router.get('/:server_id/:user_id', [getBansforUserByServer])
+router.post('/:server_id', [addBantoServer])
 export default router;

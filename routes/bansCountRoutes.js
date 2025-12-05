635c2b6d20e2a3a100e909b9da2145892a1b7b98
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-05 13:05:50 -0500

    feat: fix route handler for getting all ban counts

--- routes/bansCountRoutes.js
+++ routes/bansCountRoutes.js
@@ -3,6 +3,6 @@ const router = Router();
 import banCountController from '../controllers/banCountController.js';
 import { body, validationResult, param, query } from 'express-validator';
 
-router.get('/', banCountController);
+router.get('/', banCountController.getAllCounts);
 router.get('/:server_id/:user_id/', banCountController.getCountForUser);
 export default router;

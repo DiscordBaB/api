49391761a15dcd27bd082bc0a2b8af68d2d4503c
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-06 00:07:41 -0500

    fix: correct import and initialization of Sequelize in db.js

--- databases/db.js
+++ databases/db.js
@@ -1,12 +1,11 @@
-import SQLize from 'sequelize';
+import Sequelize from 'sequelize';
 import cfg from '/home/ken/.dbab-config/dbab.json' with {type: 'json'};
-let sequelize;
 // @ts-ignore
-sequelize = new SQLize(cfg.mysql.database, cfg.mysql.user, cfg.mysql.password,{
+const sequelize = new Sequelize(cfg.mysql.database, cfg.mysql.user, cfg.mysql.password,{
     dialect: 'mysql',
     logging: console.log,
     host: cfg.mysql.host,
     port: cfg.mysql.port
 });
 export { sequelize }
-export { SQLize}
+export { Sequelize }

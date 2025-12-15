e179bd3914cf80492cfc9cf920499a27f99de51b
Ken Spencer / IotaSpencer <me@iotaspencer.me>
2025-12-15 12:42:05 -0500

    feat: add createAppeal and add dayjs to imports/requires

--- controllers/appealController.js
+++ controllers/appealController.js
@@ -1,6 +1,6 @@
 
 import Appeal from '../models/appealModel.js';
-
+import dayjs from 'dayjs';
 export function getAllAppeals (req, res) {
     const limit = req.body.limit || 25;
     const page = req.body.page || 0;
@@ -26,5 +26,23 @@ export function deleteAppeal (req, res) {
 
     Appeal.destroy({ where: { id: appeal_id } })
     .then(deletedCount => res.status(200).json({ message: 'APPEAL_DELETED', deleted: deletedCount }))
+    .catch(err => res.status(500).send({ code: 1, message: 'SQL_ERROR' }));
+}
+export function createAppeal (req, res) {
+    const userID = req.body.userID;
+    const serverID = req.body.serverID;
+    const reason = req.body.reason;
+    const disclaimer = req.body.disclaimer;
+    const createdAt = dayjs().toISOString();
+
+    Appeal.create({
+        id: userID,
+        serverID: serverID,
+        reason: reason,
+        disclaimer: disclaimer,
+        userID: userID,
+        createdAt: createdAt
+    })
+    .then(appeal => res.status(200).json(appeal))
     .catch(err => res.status(500).send({ code: 1, message: 'SQL_ERROR' }));
 }
